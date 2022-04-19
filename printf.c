3b574bb5b43440167b664f8ccf3da41f7c72e053
TanakaMasunda <masundatanaka@gmail.com>
2022-04-19 13:44:39 -0700

    printf

--- printf.c
+++ printf.c
@@ -1,12 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _printf - function to be used to handle formats
+ * @format: format of data to be printed
+ * Return: numbers of elements in a string
+ */
+
+
 int _printf(const char *format, ...)
 {
 	char x = x;
-	char str = hope;
+	char *str = "hope";
+	{
+		printf("%c\n", x);
+		return (x);
+	}
 
-	printf("%c\n", x);
 	printf("%s\n", str);
 	printf("%\%\n");
 
