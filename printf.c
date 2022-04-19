c40204d103575779124e24edcb0e4569a845405f
TanakaMasunda <masundatanaka@gmail.com>
2022-04-19 13:35:18 -0700

    printf

---
+++ printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+int _printf(const char *format, ...)
+{
+	char x = x;
+	char str = hope;
+
+	printf("%c\n", x);
+	printf("%s\n", str);
+	printf("%\%\n");
+
+	return (0);
+}
